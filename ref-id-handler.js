1979b1748796137f9dfaf6222a631be43b0bbb57
Mikhail Varenitsyn <95382562+mvarenitsyn@users.noreply.github.com>
2024-05-27 20:36:01 -0400

    Update ref-id-handler.js

--- ref-id-handler.js
+++ ref-id-handler.js
@@ -1,3 +1,6 @@
+// Log a message to indicate that the script is loaded
+console.log("ref-id-handler.js script loaded");
+
 // Function to get URL parameters
 function getQueryParam(param) {
     const urlParams = new URLSearchParams(window.location.search);
@@ -21,31 +24,45 @@ function setCookie(name, value, days) {
 
 // Main function to check and set the cookie
 function checkAndSetRefIdCookie() {
+    console.log("checkAndSetRefIdCookie called");
     const refId = getQueryParam('ref_id');
+    console.log('ref_id from URL:', refId); // Debugging statement
     if (refId) {
         const existingCookie = getCookie('ref_id');
+        console.log('Existing cookie:', existingCookie); // Debugging statement
         if (!existingCookie) {
             setCookie('ref_id', refId, 30); // Set cookie for 30 days
+            console.log('Cookie set:', document.cookie); // Debugging statement
         }
     }
 }
 
 // Function to set ref_id value in the form's hidden field
 function setRefIdInForm() {
+    console.log("setRefIdInForm called");
     const refId = getCookie('ref_id');
+    console.log('ref_id from cookie:', refId); // Debugging statement
     if (refId) {
         const form = document.getElementById('wf-form-Lead-Form'); // Using the provided form ID
         if (form) {
-            const hiddenField = form.querySelector('input[name="ref_id"]');
+            console.log('Form found:', form); // Debugging statement
+            const hiddenField = form.querySelector('input[name="Ref_id"]');
             if (hiddenField) {
+                console.log('Hidden field found:', hiddenField); // Debugging statement
                 hiddenField.value = refId;
+                console.log('Hidden field value set:', hiddenField.value); // Debugging statement
+            } else {
+                console.log('Hidden field not found'); // Debugging statement
             }
+        } else {
+            console.log('Form not found'); // Debugging statement
         }
     }
 }
 
 // Run the functions on page load
 window.onload = function() {
+    console.log("window.onload called");
     checkAndSetRefIdCookie();
     setRefIdInForm();
 };
